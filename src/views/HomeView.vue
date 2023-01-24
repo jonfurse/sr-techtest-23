510baaa25ce806d3a02594a9190b243ac3ade6a1
jonfurse <jon@substantiveresearch.com>
2023-01-24 02:33:33 +0000

    Removed unused scaffolding code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-// import TheWelcome from '../components/TheWelcome.vue'
+import TechTest from '../components/TechTest.vue'
 </script>
 
 <template>
   <main>
-<!--    <TheWelcome />-->
+<!--    <TechTest />-->
   </main>
 </template>
